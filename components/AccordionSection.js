6ec577e2e37992f4320257f70ebf066dbbec4756
AryamanAg <44327603+AryamanAg@users.noreply.github.com>
2025-06-21 19:14:26 +0530

    Fix accordion alignment

--- components/AccordionSection.js
+++ components/AccordionSection.js
@@ -22,12 +22,25 @@ export default function AccordionSection({ title, isOpen, onToggle, children }) 
         <span
           className={`transition-transform ${isOpen ? 'rotate-180' : 'rotate-0'}`}
         >
-          ⌄
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.23 7.21a.75.75 0 011.06.02L10 11.585l3.71-4.355a.75.75 0 111.14.976l-4.25 5a.75.75 0 01-1.14 0l-4.25-5a.75.75 0 01.02-1.06z"
+              clipRule="evenodd"
+            />
+          </svg>
         </span>
       </button>
       <div
         ref={contentRef}
-        className="px-4 pb-4 overflow-hidden text-sm text-gray-700 transition-all"
+        className={`px-4 overflow-hidden text-sm text-gray-700 transition-all ${
+          isOpen ? 'pb-4' : 'pb-0'
+        }`}
         style={{ maxHeight: isOpen ? contentRef.current?.scrollHeight : 0 }}
       >
         {children}
